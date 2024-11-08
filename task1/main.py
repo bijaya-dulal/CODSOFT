8cd440bbf4021eed1776e8e625246364096d4165
bijaya-dulal <dulalbijaya0@gmail.com>
2024-11-08 21:25:14 +0545

    add input and diplay section

--- task1/main.py
+++ task1/main.py
@@ -5,5 +5,16 @@ window = tk.Tk()
 window.title('TO-DO-List')
 window.geometry('600x600')
 
+#define a task list
+to_do =[]
+
+#task  input
+entry_task =tk.Entry(window,width=40)
+entry_task.pack(pady=10)
+
+
+#list box to display task
+listbox = tk.Listbox(window,width=60, height=15,)
+listbox.pack(pady=10)
 
 window.mainloop()
