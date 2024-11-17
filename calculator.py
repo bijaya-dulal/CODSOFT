a127c3326ea3f0ae4d45fc0ad065993f14debc1d
bijaya-dulal <dulalbijaya0@gmail.com>
2024-11-17 17:27:14 +0545

    operation in loop

--- calculator.py
+++ calculator.py
@@ -1,25 +1,36 @@
-num1, num2 = map(float, input("Enter two numbers separated by space: ").split())
 
 
- 
- # Function to convert number into string
+while(1):
+
+    num1, num2 = map(float, input("Enter two numbers separated by space: ").split())
+
+
 # Switcher is dictionary data type here
-def calculate(argument):
-    switcher = {
-        '+': num1+num2,
-        '-':num1-num2,
-        '*':num1*num2,
-        '/': 'divide by 0' if num2 == 0 else num1/num2
-               
+    def calculate(argument):
+        switcher = {
+            '+': num1+num2,
+            '-':num1-num2,
+            '*':num1*num2,
+            '/': 'divide by 0' if num2 == 0 else num1/num2
+                   
+            
+        }
+    
+      
+        return switcher.get(argument, "wrong symbol")
+    
+    # Driver program
+    if __name__ == "__main__":
+        argument=input('enter the operation')
+    
+        print (calculate(argument))
+    close=input('Type Y to continue any other to close').strip().lower()
+    if close == 'y':
+        print("Continuing...")
         
-    }
+    else:
+        print("Exiting...")
+        break
 
-  
-    return switcher.get(argument, "wrong symbol")
 
-# Driver program
-if __name__ == "__main__":
-    argument=input('enter the operation')
-
-    print (calculate(argument))
-
+    
