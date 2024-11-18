480be9a70ac02439dcb51798d80b16931f27e30e
bijaya-dulal <dulalbijaya0@gmail.com>
2024-11-18 23:41:25 +0545

    pas_gen() is defined

---
+++ password_gen.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+length = int(input('enter the length of the password'))
+
+#function to generate the password of given length
+def gen_pas(len):
+    length= len
+    #define the characters  to include in password
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    random_pwd = ''.join(random.choice(characters) for _ in range(length))
+
+    return random_pwd
+print(gen_pas(length))    
+  
